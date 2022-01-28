8f85701ca1984a7de0406ea4e11e99a09cdce0f9
Marios Kiatos <kiatosm@gmail.com>
2022-01-28 12:30:25 +0200

    Add push-grasping v2 (fcn + reg)

--- ppg/agent.py
+++ ppg/agent.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import cv2
 import random
 import torch
+import math
+
 from sklearn.cluster import DBSCAN
 from sklearn.linear_model import LinearRegression
 
@@ -437,4 +439,130 @@ class HeuristicPushGrasping(PushGrasping):
         env_action['push_distance'] = self.dist
         return env_action
 
+
+class PushGrasping2(Policy):
+    def __init__(self, params):
+        super(PushGrasping2, self).__init__(params)
+
+        self.rotations = params['rotations']
+        self.aperture_limits = params['aperture_limits']
+        self.crop_size = 32
+        self.push_distance = 0.1
+        self.z = 0.1
+        self.pxl_size = 0.005
+
+        self.fcn = ResFCN().to('cuda')
+        self.reg = Regressor().to('cuda')
+
+        self.learn_step_counter = 0.0
+
+    def pre_process(self, state):
+        pass
+
+    def post_process(self, state):
+        pass
+
+    def pre_process_aperture(self, state):
+        pass
+
+    def random_sample(self, state):
+        action = np.zeros((4,))
+        action[0] = self.rng.randint(0, state.shape[0])
+        action[1] = self.rng.randint(0, state.shape[1])
+        action[2] = self.rng.randint(0, 16) * 2 * np.pi / self.rotations
+        action[3] = self.rng.uniform(self.aperture_limits[0], self.aperture_limits[1])
+        return action
+
+    def guided_exploration(self, state, sample_limits=[0.09, 0.1]):
+        obj_ids = np.argwhere(state > self.z)
+
+        # Sample initial position.
+        valid_pxl_map = np.zeros(state.shape)
+        for x in range(state.shape[0]):
+            for y in range(state.shape[1]):
+                dists = np.linalg.norm(np.array([y, x]) - obj_ids, axis=1)
+                if sample_limits[0] / self.pxl_size < np.min(dists) < sample_limits[1] / self.pxl_size:
+                    valid_pxl_map[y, x] = 255
+
+        valid_pxls = np.argwhere(valid_pxl_map == 255)
+        valid_ids = np.arange(0, valid_pxls.shape[0])
+
+        objects_mask = np.zeros(state.shape)
+        objects_mask[state > self.z] = 255
+        _, thresh = cv2.threshold(objects_mask.astype(np.uint8), 127, 255, 0)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        while True:
+            pxl = valid_pxls[random.choice(valid_ids)]
+            p1 = np.array([pxl[1], pxl[0]])
+
+            # Sample pushing direction. Push directions point always towards the objects.
+            # Keep only contour points that are around the sample pixel position.
+            pushing_area = self.push_distance / self.pxl_size
+            points = []
+            for cnt in contours:
+                for pnt in cnt:
+                    if (p1[0] - pushing_area < pnt[0, 0] < p1[0] + pushing_area) and \
+                       (p1[1] - pushing_area < pnt[0, 1] < p1[1] + pushing_area):
+                        points.append(pnt[0])
+            if len(points) > 0:
+                break
+
+        p2 = random.choice(points)
+        push_dir = p2 - p1
+        theta = -np.arctan2(push_dir[1], push_dir[0])
+        step_angle = 2 * np.pi / self.rotations
+        discrete_theta = round(theta / step_angle) * step_angle
+
+        # Sample aperture uniformly
+        aperture = self.rng.uniform(self.aperture_limits[0], self.aperture_limits[1])
+
+        action = np.zeros((4,))
+        action[0] = p1[0]
+        action[1] = p1[1]
+        action[2] = discrete_theta
+        action[3] = aperture
+        return action
+
+    def explore(self, state):
+        epsilon = self.params['epsilon_end'] + (self.params['epsilon_start'] - self.params['epsilon_end']) * \
+                  math.exp(-1 * self.learn_step_counter / self.params['epsilon_decay'])
+        self.info['epsilon'] = epsilon  # save for plotting
+
+        if self.rng.rand() < epsilon:
+            if self.rng.rand() < 0.5:
+                action = self.guided_exploration(state)
+            else:
+                action = self.random_sample(state)
+        else:
+            action = self.predict(state)
+
+        return action
+
+    def predict(self, state):
+        # action = self.random_sample(state)
+        action = self.guided_exploration(state)
+        return action
+
+    def learn(self, state, label):
+        # Store state and label to replay buffer
+
+        # Sample from replay buffer
+
+        # Update FCN model
+
+        # If label = 1 (successful grasp), update regressor
+
+        pass
+
+    def action(self, action, bounds, pxl_size):
+        # Convert from pixels to 3d coordinates.
+        x = -(pxl_size * action[0] - bounds[0][1])
+        y = pxl_size * action[1] - bounds[1][1]
+        quat = ori.Quaternion.from_rotation_matrix(np.matmul(ori.rot_y(-np.pi / 2), ori.rot_x(action[2])))
+
+        return {'pos': np.array([x, y, self.z]),
+                'quat': quat,
+                'aperture': action[3],
+                'push_distance': self.push_distance}
 
