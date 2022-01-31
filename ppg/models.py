7a21a812a5a886428fa51bb26f3bf3c0c499097c
Marios Kiatos <kiatosm@gmail.com>
2022-01-31 14:54:32 +0200

    Remove image-wide softmax for inference

--- ppg/models.py
+++ ppg/models.py
@@ -145,10 +145,10 @@ class ResFCN(nn.Module):
             out_prob = F.grid_sample(prob, flow_grid_after, mode='nearest', align_corners=True)
 
             # Image-wide softmax
-            output_shape = out_prob.shape
-            out_prob = out_prob.view(output_shape[0], -1)
-            out_prob = torch.softmax(out_prob, dim=1)
-            out_prob = out_prob.view(output_shape).to(dtype=torch.float)
+            # output_shape = out_prob.shape
+            # out_prob = out_prob.view(output_shape[0], -1)
+            # out_prob = torch.softmax(out_prob, dim=1)
+            # out_prob = out_prob.view(output_shape).to(dtype=torch.float)
 
             return out_prob
 
