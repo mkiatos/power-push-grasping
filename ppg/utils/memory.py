a148969f8297cef7dfcdd7eeac8bd5e082da8063
Marios Kiatos <kiatosm@gmail.com>
2022-02-03 15:06:11 +0200

    Add prioritized experience replay buffer

--- ppg/utils/memory.py
+++ ppg/utils/memory.py
@@ -57,3 +57,49 @@ class ReplayBuffer:
 
     def seed(self, random_seed):
         self.random.seed(random_seed)
+
+
+class PrioritizedReplayBuffer(ReplayBuffer):
+    def __init__(self, log_dir, buffer_size=100000, alpha=2):
+        super(PrioritizedReplayBuffer, self).__init__(log_dir, buffer_size)
+        self.alpha = alpha
+        self.surprise_values = []
+        self.max_priority = 0.5
+
+        if not os.path.exists(os.path.join(log_dir, 'replay_buffer')):
+            os.mkdir(os.path.join(log_dir, 'replay_buffer'))
+
+        self.z_fill = 6
+
+    def __call__(self, index):
+        return self.buffer[index]
+
+    def store(self, error, transition):
+        self.surprise_values.append(error)
+        super(PrioritizedReplayBuffer, self).store(transition)
+
+    def sample_proportional(self, batch_size):
+        sorted_surprise_ids = np.argsort(self.surprise_values)
+        rand_sample_ids = np.round(np.random.power(self.alpha, batch_size) * (self.count - 1)).astype(int)
+        ids = sorted_surprise_ids[rand_sample_ids]
+        return ids
+
+    def sample(self, given_batch_size=1):
+        if self.count < given_batch_size:
+            batch_size = self.count
+        else:
+            batch_size = given_batch_size
+
+        batch_id = self.sample_proportional(batch_size)[0]
+        print(batch_id)
+
+        folder_name = os.path.join(self.log_dir, 'replay_buffer/transition_' + str(batch_id).zfill(5))
+        print(folder_name)
+        state = cv2.imread(os.path.join(folder_name, 'heightmap.exr'), -1)
+        action = pickle.load(open(os.path.join(folder_name, 'action'), 'rb'))
+
+        return state, action, batch_id
+
+    def update_priorities(self, idxs, priorities):
+        self.surprise_values[idxs] = priorities
+        self.max_priority = np.max(priorities)
