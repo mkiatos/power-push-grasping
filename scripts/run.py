593447fde1540afdea06ba045cda0fb77372f93f
Marios Kiatos <kiatosm@gmail.com>
2022-02-04 15:29:23 +0200

    Collect random data ( for supervised learning)

--- scripts/run.py
+++ scripts/run.py
@@ -1,7 +1,4 @@
 import pickle
-
-from ppg.train import train_fcn_net, train_classifier
-
 import yaml
 import numpy as np
 import copy
@@ -12,12 +9,14 @@ import matplotlib.pyplot as plt
 
 from ppg.environment import Environment
 from ppg.agent import PushGrasping, HeuristicPushGrasping, PushGrasping2
-from ppg.utils import utils
-from ppg import cameras
+from ppg.train import train_fcn_net, train_classifier, eval_aperture_net
+from ppg.utils.utils import Logger
 
 
-def run_episode(policy, env, train=True):
+def run_episode(policy, env, episode_seed, max_steps=15, train=True):
+    env.seed(episode_seed)
     obs = env.reset()
+    print('Episode seed:', episode_seed)
 
     while not policy.init_state_is_valid(obs):
         obs = env.reset()
@@ -28,7 +27,8 @@ def run_episode(policy, env, train=True):
                     'collisions': 0,
                     'objects_removed': 0,
                     'objects_in_scene': len(obs['full_state'])}
-    while True:
+    for i in range(max_steps):
+        print('---Step:', i)
         state = policy.state_representation(obs)
 
         # Select action
@@ -42,45 +42,35 @@ def run_episode(policy, env, train=True):
         # Step environment.
         next_obs, grasp_info = env.step(env_action)
 
-        # Update logger
         episode_data['attempts'] += 1
         if grasp_info['stable']:
             episode_data['successes'] += 1
             episode_data['objects_removed'] += 1
         else:
             episode_data['fails'] += 1
-            if grasp_info['collision']:
-                episode_data['collisions'] += 1
 
         if train:
             transition = {'state': state, 'action': action, 'label': grasp_info['stable']}
             policy.learn(transition)
 
+        print(grasp_info)
+
+        # if policy.terminal(obs, next_obs) or ((not grasp_info['stable']) and grasp_info['num_contacts'] > 0):
         if policy.terminal(obs, next_obs):
             break
 
         obs = copy.deepcopy(next_obs)
 
+    print('--------')
     return episode_data
 
 
 def train_agent(log_path, n_scenes, save_every=100, seed=0):
     with open('../yaml/bhand.yml', 'r') as stream:
         params = yaml.safe_load(stream)
-
-    # Create a logger
-    if os.path.exists(log_path):
-        print('Directory ', log_path, 'exists, do you want to remove it? (y/n)')
-        answer = input('')
-        if answer == 'y':
-            shutil.rmtree(log_path)
-            os.mkdir(log_path)
-        else:
-            exit()
-    else:
-        os.mkdir(log_path)
-
     params['log_dir'] = log_path
+
+    logger = Logger(log_path)
 
     env = Environment(assets_root='../assets/', workspace_pos=[0.0, 0.0, 0.0])
 
@@ -90,15 +80,19 @@ def train_agent(log_path, n_scenes, save_every=100, seed=0):
     rng = np.random.RandomState()
     rng.seed(seed)
 
+    train_data = []
     for i in range(n_scenes):
         episode_seed = rng.randint(0, pow(2, 32) - 1)
-        env.seed(episode_seed)
-        episode_data = run_episode(policy, env, train=True)
+        episode_data = run_episode(policy, env, episode_seed, train=True)
+        train_data.append(episode_data)
+
+        logger.log_data(train_data, 'train_data')
+
         if i % save_every == 0:
             policy.save(epoch=i)
 
 
-def eval(n_scenes, log_path, seed=0):
+def eval_agent(n_scenes, log_path, seed=0):
     # Create a logger
     if os.path.exists(log_path):
         print('Directory ', log_path, 'exists, do you want to remove it? (y/n)')
@@ -116,8 +110,6 @@ def eval(n_scenes, log_path, seed=0):
 
     params['log_dir'] = log_path
 
-    pxl_size = 0.005
-    bounds = np.array([[-0.25, 0.25], [-0.25, 0.25], [0.01, 0.3]])
     env = Environment(assets_root='../assets/', workspace_pos=[0.0, 0.0, 0.0])
     # policy = PushGrasping(params)
     # policy.load(weights_fcn='../logs/fcn_model/model_15.pt',
@@ -126,7 +118,9 @@ def eval(n_scenes, log_path, seed=0):
     # policy.seed(seed)
 
     policy = PushGrasping(params)
-    policy.load('../logs/train_self_supervised/model_200')
+    # policy.load('../logs/supervised')
+    policy.load_seperately(fcn_model='../logs/fcn_model/model_5.pt',
+                           reg_model='../logs/regressor/model_10.pt')
     # policy.seed(seed)
 
     rng = np.random.RandomState()
@@ -136,52 +130,57 @@ def eval(n_scenes, log_path, seed=0):
     for i in range(n_scenes):
 
         episode_seed = rng.randint(0, pow(2, 32) - 1)
+        episode_data = run_episode(policy, env, episode_seed, train=False)
+        eval_data.append(episode_data)
+
+    pickle.dump(eval_data, open(os.path.join(log_path, 'eval_data'), 'wb'))
+
+
+def collect_random_dataset(n_scenes, log_path, seed=1):
+    with open('../yaml/bhand.yml', 'r') as stream:
+        params = yaml.safe_load(stream)
+    params['log_dir'] = log_path
+    logger = Logger(log_path)
+
+    env = Environment(assets_root='../assets/', workspace_pos=[0.0, 0.0, 0.0])
+
+    policy = PushGrasping(params)
+    policy.seed(seed)
+
+    rng = np.random.RandomState()
+    rng.seed(seed)
+
+    for j in range(n_scenes):
+        episode_seed = rng.randint(0, pow(2, 32) - 1)
         env.seed(episode_seed)
-
-        print('Episode {} seed{}:'.format(i, episode_seed))
-
         obs = env.reset()
+        print('Episode seed:', episode_seed)
+
         while not policy.init_state_is_valid(obs):
             obs = env.reset()
 
-        episode_data = {'successes': 0,
-                        'fails': 0,
-                        'attempts': 0,
-                        'objects_removed': 0,
-                        'objects_in_scene': len(obs['full_state'])}
-        while True:
+        for i in range(15):
             state = policy.state_representation(obs)
-            if len(np.argwhere(state > 0.1)) == 0:
-                break
 
-            action = policy.predict(state)
-            if action is None:
-                break
-                # TODO: why none?
-
+            # Select action
+            action = policy.guided_exploration(state)
             env_action = policy.action(action)
 
+            # Step environment.
             next_obs, grasp_info = env.step(env_action)
-            print('Success:', grasp_info['stable'])
 
-            if grasp_info['collision']:
-                continue
-
-            episode_data['attempts'] += 1
             if grasp_info['stable']:
-                episode_data['successes'] += 1
-                episode_data['objects_removed'] += 1
-            else:
-                episode_data['fails'] += 1
+                transition = {'state': state, 'action': action, 'label': grasp_info['stable']}
+                policy.replay_buffer.store(transition)
+
+            print(action)
+            print(grasp_info)
+            print('---------')
 
             if policy.terminal(obs, next_obs):
                 break
 
             obs = copy.deepcopy(next_obs)
-
-        eval_data.append(episode_data)
-
-    pickle.dump(eval_data, open(os.path.join(log_path, 'eval_data'), 'wb'))
 
 
 def analyze(log_dir):
@@ -231,26 +230,34 @@ def analyze_replay_buffer(replay_buffer_dir):
     plt.bar(discrete_apertures, heights, width=0.5)
     plt.show()
 
+
 if __name__ == "__main__":
 
-    # params = {'dataset_dir': '../logs/dataset_no_flats',
+    # params = {'dataset_dir': '../logs/train_self_supervised_per/replay_buffer',
     #           'split_ratio': 0.9,
     #           'epochs': 100,
     #           'batch_size': 1,
     #           'learning_rate': 0.0001,
     #           'log_path': '../logs/fcn_model'}
     # train_fcn_net(params)
-
-    # params = {'dataset_dir': '../logs/dataset_no_flats',
+    #
+    # params = {'dataset_dir': '../logs/self_supervised_large_dist/replay_buffer',
     #           'split_ratio': 0.9,
     #           'epochs': 100,
-    #           'batch_size': 1,
+    #           'batch_size': 4,
     #           'learning_rate': 0.0001,
-    #           'log_path': '../logs/classifier'}
+    #           'log_path': '../logs/regressor'}
     # train_classifier(params)
 
-    train_agent(log_path='../logs/train_self_supervised', n_scenes=10000, seed=0)
+    # train_agent(log_path='../logs/self-supervised', n_scenes=10000, seed=0)
 
-    # eval(n_scenes=100, log_path='../logs/eval_self_supervised', seed=1)
+    # eval(n_scenes=100, log_path='../logs/eval_supervised', seed=1)
 
     # analyze(log_dir='../logs/eval_heuristic_policy')
+
+    # analyze_replay_buffer('../logs/train_self_supervised/replay_buffer')
+
+    # eval_aperture_net(params)
+
+    collect_random_dataset(n_scenes=10000, log_path='../logs/random-dataset-2', seed=2)
+    
