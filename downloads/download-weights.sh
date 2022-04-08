c747feb9a42d1e963020133aebbbc0bf02ad8bec
Marios Kiatos <kiatosm@gmail.com>
2022-04-08 17:51:34 +0300

    Add the functionality to download the pretrained models

---
+++ downloads/download-weights.sh
@@ -0,0 +1,3 @@
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+wget https://drive.google.com/drive/folders/ppg/downloads/reg_model.pth
+wget https://drive.google.com/drive/folders/1shrAsrC0QPhlCpJCbak_l5p39X_f_g7x/fcn_model.pth
